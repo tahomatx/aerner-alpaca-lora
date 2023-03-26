ee545564dae129111520df400a88371d2a111962
Terai Kenji <kenji@tahomatx.com>
2023-03-26 22:57:54 +0900

    modify

--- finetune.py
+++ finetune.py
@@ -77,9 +77,12 @@ def train(
         device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
         gradient_accumulation_steps = gradient_accumulation_steps // world_size
 
+    print(device_map)
+
     model = LlamaForCausalLM.from_pretrained(
         base_model,
         load_in_8bit=True,
+        torch_dtype=torch.float16,
         device_map=device_map,
     )
 
@@ -151,7 +154,6 @@ def train(
     #
     model.save_pretrained(output_dir)
     print("Saved initial model")
-
 
     #
     #
