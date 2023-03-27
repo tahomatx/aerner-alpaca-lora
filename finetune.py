2bd74de612e85b4018b2ec459e8964a7dce0fd6e
Terai Kenji <kenji@tahomatx.com>
2023-03-27 19:44:41 +0900

    modify

--- finetune.py
+++ finetune.py
@@ -319,191 +319,14 @@ def train(
 
     class BetterTrainer(transformers.Trainer):
         def _wrap_model(self, model, training=True, dataloader=None):
-            if self.args.use_ipex:
-                dtype = torch.bfloat16 if self.use_cpu_amp else torch.float32
-                model = self.ipex_optimize_model(model, training, dtype=dtype)
-
-            # if is_sagemaker_mp_enabled():
-            #     # Wrapping the base model twice in a DistributedModel will raise an error.
-            #     if isinstance(self.model_wrapped, smp.model.DistributedModel):
-            #         return self.model_wrapped
-            #     return smp.DistributedModel(model, backward_passes_per_step=self.args.gradient_accumulation_steps)
-
             # already initialized its own DDP and AMP
             if self.deepspeed:
                 return self.deepspeed
-
-            # train/eval could be run multiple-times - if already wrapped, don't re-wrap it again
-            if unwrap_model(model) is not model:
-                return model
-
-            # Mixed precision training with apex (torch < 1.6)
-            if self.use_apex and training:
-                model, self.optimizer = amp.initialize(model, self.optimizer, opt_level=self.args.fp16_opt_level)
-
-            # # Multi-gpu training (should be after apex fp16 initialization)
-            # if self.args.n_gpu > 1:
-            #     model = nn.DataParallel(model)
-
-            if self.args.jit_mode_eval:
-                start_time = time.time()
-                model = self.torch_jit_model_eval(model, dataloader, training)
-                self.jit_compilation_time = round(time.time() - start_time, 4)
 
             # Note: in torch.distributed mode, there's no point in wrapping the model
             # inside a DistributedDataParallel as we'll be under `no_grad` anyways.
             if not training:
                 return model
-
-            # Distributed training (should be after apex fp16 initialization)
-            if self.sharded_ddp is not None:
-                # Sharded DDP!
-                if self.sharded_ddp == ShardedDDPOption.SIMPLE:
-                    model = ShardedDDP(model, self.optimizer)
-                else:
-                    mixed_precision = self.args.fp16 or self.args.bf16
-                    cpu_offload = ShardedDDPOption.OFFLOAD in self.args.sharded_ddp
-                    zero_3 = self.sharded_ddp == ShardedDDPOption.ZERO_DP_3
-                    # XXX: Breaking the self.model convention but I see no way around it for now.
-                    if ShardedDDPOption.AUTO_WRAP in self.args.sharded_ddp:
-                        model = auto_wrap(model)
-                    self.model = model = FullyShardedDDP(
-                        model,
-                        mixed_precision=mixed_precision,
-                        reshard_after_forward=zero_3,
-                        cpu_offload=cpu_offload,
-                    ).to(self.args.device)
-            # Distributed training using PyTorch FSDP
-            elif self.fsdp is not None:
-                if not self.args.fsdp_config["xla"]:
-                    # PyTorch FSDP!
-                    from torch.distributed.fsdp.fully_sharded_data_parallel import CPUOffload, MixedPrecision
-                    from torch.distributed.fsdp.fully_sharded_data_parallel import FullyShardedDataParallel as FSDP
-                    from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy, transformer_auto_wrap_policy
-
-                    if FSDPOption.OFFLOAD in self.args.fsdp:
-                        cpu_offload = CPUOffload(offload_params=True)
-                    else:
-                        cpu_offload = CPUOffload(offload_params=False)
-
-                    auto_wrap_policy = None
-
-                    if FSDPOption.AUTO_WRAP in self.args.fsdp:
-                        if self.args.fsdp_config["fsdp_min_num_params"] > 0:
-                            auto_wrap_policy = functools.partial(
-                                size_based_auto_wrap_policy, min_num_params=self.args.fsdp_config["fsdp_min_num_params"]
-                            )
-                        elif self.args.fsdp_config.get("fsdp_transformer_layer_cls_to_wrap", None) is not None:
-                            transformer_cls_to_wrap = set()
-                            for layer_class in self.args.fsdp_config["fsdp_transformer_layer_cls_to_wrap"]:
-                                transformer_cls = get_module_class_from_name(model, layer_class)
-                                if transformer_cls is None:
-                                    raise Exception("Could not find the transformer layer class to wrap in the model.")
-                                else:
-                                    transformer_cls_to_wrap.add(transformer_cls)
-                            auto_wrap_policy = functools.partial(
-                                transformer_auto_wrap_policy,
-                                # Transformer layer class to wrap
-                                transformer_layer_cls=transformer_cls_to_wrap,
-                            )
-                    mixed_precision_policy = None
-                    dtype = None
-                    if self.args.fp16:
-                        dtype = torch.float16
-                    elif self.args.bf16:
-                        dtype = torch.bfloat16
-                    if dtype is not None:
-                        mixed_precision_policy = MixedPrecision(param_dtype=dtype, reduce_dtype=dtype, buffer_dtype=dtype)
-                    if type(model) != FSDP:
-                        # XXX: Breaking the self.model convention but I see no way around it for now.
-                        self.model = model = FSDP(
-                            model,
-                            sharding_strategy=self.fsdp,
-                            cpu_offload=cpu_offload,
-                            auto_wrap_policy=auto_wrap_policy,
-                            mixed_precision=mixed_precision_policy,
-                            device_id=self.args.device,
-                            backward_prefetch=self.backward_prefetch,
-                            forward_prefetch=self.forword_prefetch,
-                            limit_all_gathers=self.limit_all_gathers,
-                        )
-                else:
-                    try:
-                        from torch_xla.distributed.fsdp import XlaFullyShardedDataParallel as FSDP
-                        from torch_xla.distributed.fsdp import checkpoint_module
-                        from torch_xla.distributed.fsdp.wrap import (
-                            size_based_auto_wrap_policy,
-                            transformer_auto_wrap_policy,
-                        )
-                    except ImportError:
-                        raise ImportError("Missing XLA FSDP related module; please make sure to use torch-xla >= 2.0.")
-                    auto_wrap_policy = None
-                    auto_wrapper_callable = None
-                    if self.args.fsdp_config["fsdp_min_num_params"] > 0:
-                        auto_wrap_policy = functools.partial(
-                            size_based_auto_wrap_policy, min_num_params=self.args.fsdp_config["fsdp_min_num_params"]
-                        )
-                    elif self.args.fsdp_config.get("fsdp_transformer_layer_cls_to_wrap", None) is not None:
-                        transformer_cls_to_wrap = set()
-                        for layer_class in self.args.fsdp_config["fsdp_transformer_layer_cls_to_wrap"]:
-                            transformer_cls = get_module_class_from_name(model, layer_class)
-                            if transformer_cls is None:
-                                raise Exception("Could not find the transformer layer class to wrap in the model.")
-                            else:
-                                transformer_cls_to_wrap.add(transformer_cls)
-                        auto_wrap_policy = functools.partial(
-                            transformer_auto_wrap_policy,
-                            # Transformer layer class to wrap
-                            transformer_layer_cls=transformer_cls_to_wrap,
-                        )
-                    fsdp_kwargs = self.args.xla_fsdp_config
-                    if self.args.fsdp_config["xla_fsdp_grad_ckpt"]:
-                        # Apply gradient checkpointing to auto-wrapped sub-modules if specified
-                        def auto_wrapper_callable(m, *args, **kwargs):
-                            return FSDP(checkpoint_module(m), *args, **kwargs)
-
-                    # Wrap the base model with an outer FSDP wrapper
-                    self.model = model = FSDP(
-                        model,
-                        auto_wrap_policy=auto_wrap_policy,
-                        auto_wrapper_callable=auto_wrapper_callable,
-                        **fsdp_kwargs,
-                    )
-
-                    # Patch `xm.optimizer_step` should not reduce gradients in this case,
-                    # as FSDP does not need gradient reduction over sharded parameters.
-                    def patched_optimizer_step(optimizer, barrier=False, optimizer_args={}):
-                        loss = optimizer.step(**optimizer_args)
-                        if barrier:
-                            xm.mark_step()
-                        return loss
-
-                    xm.optimizer_step = patched_optimizer_step
-            # elif is_sagemaker_dp_enabled():
-            #     model = nn.parallel.DistributedDataParallel(
-            #         model, device_ids=[int(os.getenv("SMDATAPARALLEL_LOCAL_RANK"))]
-            #     )
-            elif self.args.local_rank != -1:
-                kwargs = {}
-                if self.args.ddp_find_unused_parameters is not None:
-                    kwargs["find_unused_parameters"] = self.args.ddp_find_unused_parameters
-                elif isinstance(model, PreTrainedModel):
-                    # find_unused_parameters breaks checkpointing as per
-                    # https://github.com/huggingface/transformers/pull/4659#issuecomment-643356021
-                    kwargs["find_unused_parameters"] = not model.is_gradient_checkpointing
-                else:
-                    kwargs["find_unused_parameters"] = True
-
-                if self.args.ddp_bucket_cap_mb is not None:
-                    kwargs["bucket_cap_mb"] = self.args.ddp_bucket_cap_mb
-                if is_torch_neuroncore_available():
-                    return model
-                model = nn.parallel.DistributedDataParallel(
-                    model,
-                    device_ids=[self.args.local_rank] if self.args._n_gpu != 0 else None,
-                    output_device=self.args.local_rank if self.args._n_gpu != 0 else None,
-                    **kwargs,
-                )
 
             # torch.compile() needs to be called after wrapping the model with FSDP or DDP
             # to ensure that it accounts for the graph breaks required by those wrappers
@@ -522,8 +345,8 @@ def train(
             # else:
             #     labels = None
 
-            print(model)
             outputs = model_forward(model, inputs)
+            print(outputs)
             # outputs = model(**inputs.to("cuda:0"))
             # Save past state if it exists
             # TODO: this needs to be fixed and made cleaner later.
