4d5e774df67f62c9f6f99227dc8df1c269eb0710
Terai Kenji <kenji@tahomatx.com>
2023-03-27 20:02:55 +0900

    modify

--- finetune.py
+++ finetune.py
@@ -336,33 +336,13 @@ def train(
             return model
 
         def compute_loss(self, model, inputs, return_outputs=False):
-            """
-            How the loss is computed by Trainer. By default, all models return the loss in the first element.
-            Subclass and override for custom behavior.
-            """
             if self.label_smoother is not None and "labels" in inputs:
                 labels = inputs.pop("labels")
             else:
                 labels = None
 
-            print(inputs)
-            # input_ids, labels = inputs
             outputs = model_forward(model, inputs['input_ids'])
-            print(unwrap_model(model)._get_name())
-
-            if labels is not None:
-                if unwrap_model(model)._get_name() in MODEL_FOR_CAUSAL_LM_MAPPING_NAMES.values():
-                    loss = self.label_smoother(outputs, labels, shift_labels=True)
-                else:
-                    loss = self.label_smoother(outputs, labels)
-            else:
-                if isinstance(outputs, dict) and "loss" not in outputs:
-                    raise ValueError(
-                        "The model did not return a loss from the inputs, only the following keys: "
-                        f"{','.join(outputs.keys())}. For reference, the inputs it received are {','.join(inputs.keys())}."
-                    )
-                # We don't use .loss here since the model may return tuples instead of ModelOutput.
-                loss = outputs["loss"] if isinstance(outputs, dict) else outputs[0]
+            loss = self.label_smoother(outputs, labels, shift_labels=True)
 
             return (loss, outputs) if return_outputs else loss
 
