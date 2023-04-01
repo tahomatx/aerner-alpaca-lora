b2985a93018a755f55cc72430eba1feca8ede4da
Terai Kenji <kenji@tahomatx.com>
2023-04-01 23:19:39 +0900

    modify

--- bbb.py
+++ bbb.py
@@ -2,8 +2,8 @@
 import transformers
 import peft
 
-all_args, prompt_tuning_config, prefix_tuning_config, prompt_encoding_config, lora_config = transformers.HfArgumentParser(
-    (peft.PromptTuningConfig, peft.PrefixTuningConfig, peft.PromptEncoderConfig, peft.LoraConfig)).parse_args_into_dataclasses()
 
-print(all_args, prompt_tuning_config, prefix_tuning_config,
-      prompt_encoding_config, lora_config)
+a_args = transformers.HfArgumentParser((peft.PromptTuningConfig)).parse_args_into_dataclasses()
+b_args = transformers.HfArgumentParser((peft.LoraConfig)).parse_args_into_dataclasses()
+
+print(a_args, b_args)
