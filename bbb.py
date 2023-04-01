fc4cb2a0217fc43140d9a95272c2a95682b56194
Terai Kenji <kenji@tahomatx.com>
2023-04-01 23:59:10 +0900

    modify

--- bbb.py
+++ bbb.py
@@ -5,6 +5,15 @@ import datasets
 from dataclasses import dataclass, field
 
 
+@dataclass
+class PeftArguments:
+    peft_type: str = field()
+    r: int = field()
+    lora_alpha: int = field(),
+    lora_dropout: float = field(),
+    # Used for prompt tuning, prefix tuning and p-tuning
+    num_virtual_tokens: int = field()
+    mapping_hidden_dim: int = field()
 
 @dataclass
 class PeftArguments(peft.LoraConfig, peft.PromptTuningConfig):
