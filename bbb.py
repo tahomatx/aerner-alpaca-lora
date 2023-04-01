58837c1225a97da7637c38dbcc8b918bb19a4477
Terai Kenji <kenji@tahomatx.com>
2023-04-02 00:01:24 +0900

    modify

--- bbb.py
+++ bbb.py
@@ -3,17 +3,18 @@ import transformers
 import peft
 import datasets
 from dataclasses import dataclass, field
+from typing import Optional
 
 
 @dataclass
 class PeftArguments:
     peft_type: str = field()
-    r: int = field()
-    lora_alpha: int = field()
-    lora_dropout: float = field()
+    r: dataclass[int] = field()
+    lora_alpha: dataclass[int] = field()
+    lora_dropout: dataclass[float] = field()
     # Used for prompt tuning, prefix tuning and p-tuning
-    num_virtual_tokens: int = field()
-    mapping_hidden_dim: int = field()
+    num_virtual_tokens: dataclass[int] = field()
+    mapping_hidden_dim: dataclass[int] = field()
 
 # @dataclass
 # class PeftArguments(peft.LoraConfig, peft.PromptTuningConfig):
