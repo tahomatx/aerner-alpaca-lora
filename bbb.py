c9e5ec49da30fc77f88da409e4646c1fe6cb0043
Terai Kenji <kenji@tahomatx.com>
2023-04-01 23:35:01 +0900

    modify

--- bbb.py
+++ bbb.py
@@ -1,10 +1,17 @@
 
 import transformers
 import peft
+import datasets
+from dataclasses import dataclass, field
 
+
+
+@dataclass
+class PeftArguments(peft.LoraConfig, peft.PromptTuningConfig):
+    pass
 
 # a_args = transformers.HfArgumentParser((peft.PromptTuningConfig)).parse_args_into_dataclasses()
-arg_parser = transformers.HfArgumentParser((peft.LoraConfig))
+arg_parser = transformers.HfArgumentParser((PeftArguments))
 aaa = arg_parser.parse_args_into_dataclasses()
 
 print(aaa)
