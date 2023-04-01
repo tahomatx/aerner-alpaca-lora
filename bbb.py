9e333a636c00dc71242fca961957bb1aa5e6046e
Terai Kenji <kenji@tahomatx.com>
2023-04-02 00:17:10 +0900

    modify

--- bbb.py
+++ bbb.py
@@ -21,7 +21,7 @@ class PeftArguments:
 #     pass
 
 # a_args = transformers.HfArgumentParser((peft.PromptTuningConfig)).parse_args_into_dataclasses()
-arg_parser = transformers.HfArgumentParser((peft.LoraConfig))
+arg_parser = transformers.HfArgumentParser((transformers.TrainingArgument))
 aaa = arg_parser.parse_args_into_dataclasses()
 
 print(aaa)
